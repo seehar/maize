54da2ef100b50deab1ac8ceca6f25138bb5f7dd5
seehar <seehar@qq.com>
2025-06-18 11:44:58 +0800

    feat: mommand

--- tests/test_command/test_code_generate.py
+++ tests/test_command/test_code_generate.py
@@ -1,3 +1,5 @@
+import shutil
+
 from maize.command.code_generate import CodeGenerate
 
 
@@ -5,3 +7,5 @@ class TestCodeGenerate:
     def test_generate(self):
         code_generate = CodeGenerate()
         code_generate.generate("baidu_spider", "https://www.baidu.com")
+        # 删除创建的目录
+        shutil.rmtree("baidu_spider")
